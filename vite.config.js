e8bc3b2012c05c861f6ffaefc2ddb572155fa071
fezanqadirbhatti <fezanqadirbhatti@gmail.com>
2025-01-18 09:54:58 -0800

    .gitignore updated. Docker support added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Listen on all network interfaces
+    port: 3000, // Specify the port number
+  },
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx'], // Ensure .jsx is recognized
+    extensions: [".js", ".jsx"], // Ensure .jsx is recognized
   },
-})
+});
